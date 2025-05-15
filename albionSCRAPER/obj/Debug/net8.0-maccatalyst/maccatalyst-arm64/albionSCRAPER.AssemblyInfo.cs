c439758ea1d961c16f2d1954c1b4c7e31903b442
Adrian Stanisz <adi.stanisz@gmail.com>
2025-05-15 09:05:10 +0200

    ccc

--- albionSCRAPER/obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/albionSCRAPER.AssemblyInfo.cs
+++ albionSCRAPER/obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/albionSCRAPER.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("albionSCRAPER")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e1babda6ade01cc6fc21d20f88426c419a504d02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3b31c4f92458163cdd91f9cc0de4124c743a8f0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("albionSCRAPER")]
 [assembly: System.Reflection.AssemblyTitleAttribute("albionSCRAPER")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
